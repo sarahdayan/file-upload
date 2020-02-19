5e616e7ff5edba18f06114db5b73939be45ce685
Sarah Dayan <dayan.sarah@gmail.com>
2020-02-19 13:59:59 +0100

    feat: implement the file loader component

--- src/components/FileLoader.vue
+++ src/components/FileLoader.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <h2 class="title">Uploading {{ files.length }} files</h2>
+    <span class="size">
+      {{ filesize }}
+    </span>
+    <div class="progressbar" :style="{ width: `${progression}%` }"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+import getPowerAndUnit from "@/utils/getPowerAndUnit";
+
+export default {
+  props: {
+    files: {
+      type: Array
+    },
+    progression: {
+      type: Number
+    }
+  },
+  computed: {
+    filesize() {
+      const totalSize = this.files.reduce((acc, { size }) => acc + size, 0);
+      const { power, unit } = getPowerAndUnit(totalSize);
+
+      return totalSize / 10 ** power + " " + unit;
+    }
+  }
+};
 </script>
--- tests/unit/FileLoader.spec.js
+++ tests/unit/FileLoader.spec.js
@@ -3,12 +3,72 @@ import FileLoader from "@/components/FileLoader";
 
 let wrapper = null;
 
+const files = [
+  {
+    name: "Document.pdf",
+    size: 3200000
+  },
+  {
+    name: "Document_2.pdf",
+    size: 4300000
+  }
+];
+
 beforeEach(() => {
-  wrapper = shallowMount(FileLoader);
+  wrapper = shallowMount(FileLoader, {
+    propsData: {
+      files,
+      progression: 50
+    }
+  });
 });
 
 afterEach(() => {
   wrapper.destroy();
 });
 
-describe("FileLoader", () => {});
+describe("FileLoader", () => {
+  it("renders how many files are being uploaded", () => {
+    expect(wrapper.find(".title").text()).toBe("Uploading 2 files");
+  });
+  it("renders the total file size", () => {
+    expect(wrapper.find(".size").text()).toBe("7.5 MB");
+  });
+  it("renders the progression as a progressbar", () => {
+    expect(wrapper.find(".progressbar").attributes("style")).toContain(
+      "width: 50%"
+    );
+  });
+  it("renders the size as kilobytes when under 1000", () => {
+    wrapper = shallowMount(FileLoader, {
+      propsData: {
+        files: [
+          {
+            name: "Document.pdf",
+            size: 320
+          },
+          {
+            name: "Document_2.pdf",
+            size: 430
+          }
+        ]
+      }
+    });
+
+    expect(wrapper.find(".size").text()).toBe("0.75 KB");
+  });
+  it("renders the size as gigabytes when over 9999999", () => {
+    wrapper = shallowMount(FileLoader, {
+      propsData: {
+        files: [
+          {
+            name: "Document.pdf",
+            size: 9000000000
+          }
+        ]
+      }
+    });
+
+    expect(wrapper.find(".size").text()).toBe("9 GB");
+  });
+});
